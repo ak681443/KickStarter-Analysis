eb7e0c1d2fa179b83f7abbdd3f8a71472996f2d7
eshine2017 <deng.pan.sjtu@gmail.com>
2018-03-29 19:34:19 -0400

    create authenticate servlet

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -1,30 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import="java.sql.PreparedStatement"%>
-<%@ page import="java.sql.ResultSet"%>
-<%@ page import="java.sql.DriverManager"%>
-<%@ page import="java.sql.Connection"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Log in</title>
+<title>Login</title>
 </head>
 
 <body>
 
-<%
-Class.forName("oracle.jdbc.driver.OracleDriver");  
-Connection con=DriverManager.getConnection(  
-"jdbc:oracle:thin:@oracle.cise.ufl.edu:1521:orcl","dpan", "pddp941213");  
-
-PreparedStatement pstm = con.prepareStatement("select * from SSHI.USERS");
-ResultSet rs = pstm.executeQuery();
-
-%>
-
-<form method="POST" action="Testing" style="margin: 50px">
+<form method="POST" action="login" style="margin: 50px">
 <table align="center">
 	<tr>
 		<td colspan="2" align="center"><em>Login to KickStarter</em></td>
---
+++ src/org/dbms/ks/Authentication.java
@@ -0,0 +1,43 @@
+package org.dbms.ks;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Authentication
+ */
+@WebServlet("/login")
+public class Authentication extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authentication() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		res.setContentType("text/html");//setting the content type  
+		PrintWriter pw=res.getWriter();//get the stream to write the data  
+		  
+		//writing html in the stream  
+		pw.println("<html><body>");  
+		pw.println("Welcome to servlet");  
+		pw.println("</body></html>");  
+		  
+		pw.close();//closing the stream  
+	}
+
+}
